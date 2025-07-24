8eadfa47c5caedf11fe5075399f1a254c084cc0a
adhithyapatel <adhithyapatel6@gmail.com>
2025-07-24 21:12:11 +0530

    Delete Code directory

--- Code/application/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import candidate,voter,cast_vote,election_name
-
-admin.site.register(candidate)
-admin.site.register(voter)
-admin.site.register(cast_vote)
-admin.site.register(election_name)
